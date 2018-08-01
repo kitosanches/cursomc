2b957ae86878d57a5cdc8a6875fbdb2a44cbdc86
Marcus Sanches <marcus_sanchesant@hotmail.com>
2018-08-01 07:13:42 -0300

    Testando Resource e primeira classe de dominio

---
+++ src/main/java/com/marcus/cursomc/domain/Categoria.java
@@ -0,0 +1,46 @@
+package com.marcus.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	int id;
+	String nome;
+
+	public Categoria(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public boolean equals(Object arg0) {
+		// TODO Auto-generated method stub
+		return super.equals(arg0);
+	}
+
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+
+}
