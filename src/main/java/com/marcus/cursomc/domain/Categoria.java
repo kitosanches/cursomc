5ee4203171176f119f3df26becefa8cdd2a7c9b9
Marcus Sanches <marcus_sanchesant@hotmail.com>
2018-08-04 09:05:28 -0300

    Repository and Service

--- src/main/java/com/marcus/cursomc/domain/Categoria.java
+++ src/main/java/com/marcus/cursomc/domain/Categoria.java
@@ -2,24 +2,34 @@ package com.marcus.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
-	int id;
+@Id
+@GeneratedValue (strategy = GenerationType.IDENTITY)
+	Integer id;
 	String nome;
+	
 
-	public Categoria(int id, String nome) {
+	public Categoria() {
+	}
+
+	public Categoria(Integer id, String nome) {
 		super();
 		this.id = id;
 		this.nome = nome;
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -32,15 +42,34 @@ public class Categoria implements Serializable {
 	}
 
 	@Override
-	public boolean equals(Object arg0) {
-		// TODO Auto-generated method stub
-		return super.equals(arg0);
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
 	}
 
 	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
 	}
-
+	
 }
--- src/main/java/com/marcus/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/marcus/cursomc/resources/CategoriaResource.java
@@ -1,14 +1,21 @@
 package com.marcus.cursomc.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.marcus.cursomc.services.CategoriaService;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
- @RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "funfou";
+	@Autowired
+	private CategoriaService categoriaService;
+ @RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+	 return ResponseEntity.ok(this.categoriaService.buscar(id));
 	}
 }
