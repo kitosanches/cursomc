5469754b0289828cb05f0e642c8f8ac1a89108df
Marcus Sanches <marcus_sanchesant@hotmail.com>
2018-11-04 10:56:53 -0200

    metodo post de cliente

--- src/main/java/com/marcus/cursomc/resources/ClienteResource.java
+++ src/main/java/com/marcus/cursomc/resources/ClienteResource.java
@@ -1,15 +1,19 @@
 package com.marcus.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marcus.cursomc.domain.Categoria;
 import com.marcus.cursomc.domain.Cliente;
 import com.marcus.cursomc.services.ClienteService;
 
@@ -29,5 +33,12 @@ public class ClienteResource {
 	public ResponseEntity<List<Cliente>> findAll(){
 		 return ResponseEntity.ok(this.clienteService.findAll());
 		}
+	
+	 @RequestMapping(method =RequestMethod.POST)
+	 public ResponseEntity<Void> insert (@RequestBody Cliente obj) {
+		 obj = clienteService.insert(obj);
+		 URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		 return ResponseEntity.created(uri).build();
+	 }
 
 }
--- src/main/java/com/marcus/cursomc/services/ClienteService.java
+++ src/main/java/com/marcus/cursomc/services/ClienteService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.marcus.cursomc.domain.Categoria;
 import com.marcus.cursomc.domain.Cliente;
 import com.marcus.cursomc.repositories.ClienteRepository;
 import com.marcus.cursomc.services.exception.ObjectNotFoundException;
@@ -21,5 +22,9 @@ public class ClienteService {
 				}
 	public List<Cliente> findAll () {
 		return repo.findAll();
-	}	
+	}
+	public Cliente insert (Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
 }
